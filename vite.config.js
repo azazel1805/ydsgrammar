944146ac16786bb7528d5f81dcc344fd51b58cea
Adınız Soyadınız <onurtosuner@gmail.com>
2026-03-07 01:14:24 +0300

    refactor(build): integrated tailwind directly into vite using plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    plugins: [tailwindcss()],
     publicDir: 'public',
     build: {
         outDir: 'dist',
